1f3a3990d51eef0f4572b1fc61ca3c9a3d2d464a
Luis Gomez Amado <l.gomeza@uniandes.edu.co>
2021-08-21 14:21:52 -0500

    Added documentation to make code understandable

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const http = require("http");
 const axios = require("axios");
 const fs = require("fs");
 
+//server related constants
 const hostname = "localhost";
 const port = 8081;
 
+//constants for data urls
 const urlProveedores =
   "https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json";
 const urlClientes =
@@ -15,9 +17,16 @@ const urls = {
   clientes: urlClientes,
   proveedores: urlProveedores,
 };
+
+//routes constants
 const clientesRoute = "clientes";
 const proveedoresRoute = "proveedores";
 
+/**
+ * Receives a url and gets the data out of the fetched JSON response (uses axios)
+ * @param  {String} url The url where the needed data is stored
+ * @return {Object} Returns the data property of the response.
+ */
 async function get(url) {
   try {
     const response = await axios.get(url);
@@ -28,6 +37,13 @@ async function get(url) {
   }
 }
 
+/**
+ * Function that processes the information found on res depending on what type of data is being received
+ * and creates the HTML file using the necessary properties.
+ * @param  {Object} res The object that contains the data received from the data source
+ * @param  {String} type The type of data that is being received. In this case, it is either "clientes" or "proveedores"
+ * @return {String} Returns the processed HTML text.
+ */
 function handleResponse(res, type) {
   const htmlUrl = __dirname + "/index.html";
   let htmlContent = fs.readFileSync(htmlUrl, { encoding: "utf-8" });
@@ -59,6 +75,11 @@ function handleResponse(res, type) {
   return htmlContent;
 }
 
+/**
+ * Function that processes every request sent by the client and sends back a response.
+ * @param  {http.ClientRequest} req The object containing the client request
+ * @param  {http.ServerResponse} res The object that will be sent as the server response.
+ */
 async function handleRequest(req, res) {
   res.statusCode = 200;
 
